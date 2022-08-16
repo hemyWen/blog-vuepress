c23955496a2520a9b6aea03329f2ffbc50b6bc85
whm <1135063561@qq.com>
2022-08-16 09:04:22 +0800

    修改

--- deploy.sh
+++ deploy.sh
@@ -14,10 +14,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io  USERNAME=你的用户名 
-git remote add origin https://github.com/hemyWen/hemyWen.github.io.git
-git remote set-url origin https://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@github.com/hemyWen/hemyWen.github.io.git 
 git push -f https://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@github.com/hemyWen/hemyWen.github.io.git 
-git push
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>  REPO=github上的项目
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
